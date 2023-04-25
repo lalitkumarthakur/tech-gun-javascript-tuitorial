6111825d21635b74b95fceffd962f82f0c0fdcbc
kumarlalitearns6767 <45649399+kumarlearns6767@users.noreply.github.com>
2023-04-25 14:34:24 +0530

    ifelse-complete

--- begineers-part-1/ch-6_conditional_statements/greatest_numbersOf3/script.js
+++ begineers-part-1/ch-6_conditional_statements/greatest_numbersOf3/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
-const firstName = document.getElementById("fnumber").value;
+const a = prompt(`Enter the 1st number = `);
+const b = prompt(`Enter the 2nd number = `);
+const c = prompt(`Enter the 3rd number = `);
 
-console.log(firstName.value);
+if (a > b) {
+	console.log(`${a} is greater than ${b}`);
+} else if (b > c) {
+	console.log(`${b} is greater than ${c}`);
+} else if (c > a) {
+	console.log(`${c} is greater than ${a}`);
+} else {
+	alert("Invalid enter! Please try again later.");
+}
