b3fe29a941623aa810e67b1d96be4fe2a5e44ab5
kumarlalitearns6767 <45649399+kumarlearns6767@users.noreply.github.com>
2023-04-28 13:29:55 +0530

    finished-greatest-number-program

--- begineers-part-1/ch-6_conditional_statements/greatest_numbersOf3/script.js
+++ begineers-part-1/ch-6_conditional_statements/greatest_numbersOf3/script.js
@@ -1,15 +1,31 @@
 "use strict";
 
-const a = prompt(`Enter the 1st number = `);
-const b = prompt(`Enter the 2nd number = `);
-const c = prompt(`Enter the 3rd number = `);
+// const a = prompt(`Enter the 1st number = `);
+// const b = prompt(`Enter the 2nd number = `);
+// const c = prompt(`Enter the 3rd number = `);
 
-if (a > b) {
-	console.log(`${a} is greater than ${b}`);
-} else if (b > c) {
-	console.log(`${b} is greater than ${c}`);
-} else if (c > a) {
-	console.log(`${c} is greater than ${a}`);
-} else {
-	alert("Invalid enter! Please try again later.");
+// if (a > b) {
+// 	console.log(`${a} is greater than ${b}`);
+// } else if (b > c) {
+// 	console.log(`${b} is greater than ${c}`);
+// } else if (c > a) {
+// 	console.log(`${c} is greater than ${a}`);
+// } else {
+// 	alert("Invalid enter! Please try again later.");
+// }
+
+function biggest_number() {
+	let fnumber = document.getElementById("fnumber").value;
+	let snumber = document.getElementById("snumber").value;
+	let tnumber = document.getElementById("tnumber").value;
+
+	if (fnumber > snumber) {
+		alert(`${fnumber} is greater than ${snumber}`);
+	} else if (snumber > tnumber) {
+		alert(`${snumber} is greater than ${tnumber}`);
+	} else if (tnumber > fnumber) {
+		alert(`${tnumber} is greater than ${fnumber}`);
+	} else {
+		alert("Invalid enter! Please try again later.");
+	}
 }
